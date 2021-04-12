0230eff22eaa3eda3da56cfcb37f86e992e54a78
Eagalaivan <eagalaivanram@gmail.com>
2021-04-12 12:47:54 +0530

    joblistings

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-from fastapi import FastAPI
+from fastapi import Depends,FastAPI
+from sqlalchemy.orm import Session
+from .database import SessionLocal,engine
+from .models import *
+from pydantic import BaseModel
 
 app=FastAPI()
 
+Base.metadata.create_all(bind=engine)
+class JobModel(BaseModel):
+    id: int
+
+
+def get_db():
+    try:
+        db=SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def index():
     return {"index page":"Content"}
+@app.get("/jobs/")
+def show_jobs( db: Session = Depends(get_db)):
+    res=db.execute("SELECT * FROM jobs")
+    result=res.fetchall()
+    print(result)
+    db.commit()
+    return {"query":result}
 
-@app.get("/jobs/{id}")
-def show_jobs(id: int):
-    return {"id":id}
 @app.post("/jobs/{id}")
 def create_job(id:int):
-     return {"id":id}
+     pass
 
 @app.delete("/jobs/{id}")
 def delete_job(id:int):
-     return {"id":id}
+     pass
---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from .database import Base
+
+
+
+
+class Jobs(Base):
+    __tablename__ = "jobs"
+    id = Column(Integer, primary_key=True, index=True)
+    job_description = Column(String)
+    is_active = Column(Boolean, default=True)
+
+class Candidates(Base):
+    __tablename__ = "candidates"
+    c_id = Column(Integer, primary_key=True, index=True)
+    c_name = Column(String)
+    c_degree =Column(String)
+    id = Column(Integer, ForeignKey("jobs.id"))
+class Jobapplications(Base):
+    __tablename__="jobapplication"
+    job_application_id=Column(Integer,primary_key=True,index=True)
+    j_id=Column(Integer,ForeignKey("candidates.c_id"))    
